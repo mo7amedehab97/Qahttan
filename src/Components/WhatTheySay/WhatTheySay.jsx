f23eed2a8f499d101d2f4610e5fea4043ff6d253
mo7amedehab97 <gazacityface@gmail.com>
2023-01-17 01:21:13 +0200

    swiper responsive

--- src/Components/WhatTheySay/WhatTheySay.jsx
+++ src/Components/WhatTheySay/WhatTheySay.jsx
@@ -1,10 +1,11 @@
+import { useEffect, useState } from "react";
 import { Pagination } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css/bundle";
 import "swiper/css";
 import "swiper/css/pagination";
 import "./index.css";
-import QuoteCard from './QuoteCard'
+import QuoteCard from "./QuoteCard";
 const quotes = [
   {
     name: "محمد",
@@ -28,16 +29,35 @@ const quotes = [
   },
 ];
 const WhatTheySay = () => {
+  const [pageCount, setPageCount] = useState(3);
+  useEffect(() => {
+    const width = window.innerWidth;
+    setPageCount(width);
+  }, [pageCount]);
+  console.log(pageCount);
   return (
     <section className="what_they_say_container">
       <Swiper
-        slidesPerView={3}
         spaceBetween={30}
         pagination={{
           clickable: true,
         }}
         modules={[Pagination]}
         className="mySwiper"
+        breakpoints={{
+          // when window width is >= 640px
+          640: {
+            slidesPerView: 1,
+          },
+          // when window width is >= 768px
+          768: {
+            slidesPerView: 2,
+          },
+          // when window width is >= 1200px
+          1200: {
+            slidesPerView: 3,
+          },
+        }}
       >
         {quotes.length &&
           quotes.map((quote) => (
